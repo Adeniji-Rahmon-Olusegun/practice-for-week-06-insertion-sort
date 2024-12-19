71694e4a0246979ad3c4408fa1894f36d4afa23f
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-12-19 19:47:59 +0100

    insert sort

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,24 +1,36 @@
 // Insertion Sort out-of-place
 // Do not modify the original array
 function insertionSort(arr) {
-  /*
-  Pseudocode:
 
-  Copy the original array
-  Create an array to store the sorted values
-  While the array is not empty:
-  - make sure you have a console.log(sorted.join(',')) as your first line in the while loop
-  - Pop a value from the array
-  - Create a new spot at the end of the array with null to help with comparisons
-  - Walk through the sorted array in reverse order
-  - Check if the value to the left is smaller than the new value
-  - If so, you've reached the insertion point so exit the loop
-  - If not shift the value to the right by 1 and continue
-  - Insert the unsorted value at the break point
-  Return the sorted array
-  */
+  let sortedArr = [];
+  let arrCopy = arr.slice();
 
-  // Your code here
+  while (arrCopy.length > 0) {
+    console.log(sortedArr.join(','));
+
+    let popped = arrCopy.pop();
+
+    if (sortedArr.length === 0) {
+      //sortedArr.push(null);
+      sortedArr.push(popped);
+    } else {
+      if (popped > sortedArr[0]) {
+        for (let idx = 0; idx < sortedArr.length; idx++) {
+          let num = sortedArr[idx];
+
+          if (popped < num) {
+            sortedArr.splice(idx, 0, popped);
+            break;
+          }
+        }
+      } else {
+        sortedArr.splice(0, 0, popped);
+      }
+    }
+
+  }
+  
+  return sortedArr;
 }
 
 // In-place Insertion Sort
@@ -41,6 +53,23 @@ function insertionSortInPlace(arr) {
   */
 
   // Your code here
+  for (let idx = 1; idx < arr.length; idx++) {
+    let temp = arr[idx];
+    let jdx = idx - 1;
+
+    while (jdx >= 0 && arr[jdx] > temp) {
+      
+      arr[jdx + 1] = arr[jdx];
+      jdx--;
+    }
+
+    arr[jdx + 1] = temp;
+    console.log(arr.join(","));
+  }
 }
+
+arr = [2,4,6,8,1,3,5,7,9];
+
+console.log(insertionSortInPlace(arr));
 
 module.exports = [insertionSort, insertionSortInPlace];
